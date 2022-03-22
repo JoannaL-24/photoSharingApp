d06f8d994f426463ef86df6ad7c1c25fbe3e7c4c
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2022-03-22 14:34:28 +0800

    Update connectSever.php

--- connectSever.php
+++ connectSever.php
@@ -12,9 +12,9 @@
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$databaseName", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // echo "<div class=\"card\">
-        //         Connected Successfully
-        //     </div>";
+        echo "<div class=\"card\">
+                Connected Successfully
+            </div>";
     }catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
