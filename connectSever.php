0d950856fc38ebd16ed631409e4e61cc54bc4c67
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2022-03-22 14:30:58 +0800

    connect clearDB

--- connectSever.php
+++ connectSever.php
@@ -1,8 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "Joanna24*";
-    $databaseName = "photosharingapp";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "Joanna24*";
+    // $databaseName = "photosharingapp";
+    $servername = "us-cdbr-east-05.cleardb.net";
+    $username = "bcf2d653cd78c2";
+    $password = "5eef9235";
+    $databaseName = "heroku_dd4d001e84cca6c";
+    $severURL = "mysql://bcf2d653cd78c2:5eef9235@us-cdbr-east-05.cleardb.net/heroku_dd4d001e84cca6c?reconnect=true";
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$databaseName", $username, $password);
